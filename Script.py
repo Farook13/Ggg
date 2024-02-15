b40c874f026c312bc519106eea97284c427e326c
CHXRITH <141550562+CHXRITH@users.noreply.github.com>
2024-02-15 02:40:18 -0800

    Update Script.py

--- Script.py
+++ Script.py
@@ -210,8 +210,7 @@ ToTAL DISK: {}
 USED DISK: {} ({}%)
 FREE DISK: {}"""
     
-    BUTTON_LOCK_TEXT = ""Hey {query}
-This Is Not For U. Search Ur Self""
+    BUTTON_LOCK_TEXT = ""Hey {query}\nThis Is Not For U. Search Ur Self""
    
     FORCE_SUB_TEXT = "SoRRY BRo YoUR NoT JoINED MY CHANNEL So PLEAsE CLIcK JoIN BUTToN To JoIN MY CHANNEL AND TRY AGAIN"
    
