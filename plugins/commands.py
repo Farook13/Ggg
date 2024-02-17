28715c1253ad2ec99b62f77226358fb3a1304017
CHXRITH <141550562+CHXRITH@users.noreply.github.com>
2024-02-17 00:17:56 -0800

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -46,20 +46,33 @@ async def start(client, message):
         await message.reply_photo(photo=random.choice(PICS), caption=START_MESSAGE.format(user=message.from_user.mention, bot=client.mention), reply_markup=InlineKeyboardMarkup(buttons), parse_mode=enums.ParseMode.HTML)
         return await m.delete()
         
-    if AUTH_CHANNEL and not await is_subscribed(client, message):
-        try:
-            invite_link = await client.create_chat_invite_link(int(AUTH_CHANNEL))
-        except ChatAdminRequired:
-            logger.error("MAKE SURE BOT IS ADMIN IN FORCESUB CHANNEL")
-            return
-        btn = [[InlineKeyboardButton("✨ Join My Channel", url=invite_link.invite_link)]]
-        if message.command[1] != "subscribe":
+        channels = ['-1001882687525', '1195733628', '-1001786807217']
+        
+        if AUTH_CHANNEL and not await is_subscribed(client, message):
             try:
-                kk, file_id = message.command[1].split("_", 1)
-                pre = 'checksubp' if kk == 'filep' else 'checksub' 
-                btn.append([InlineKeyboardButton("⟳ Tʀʏ Aɢᴀɪɴ", callback_data=f"{pre}#{file_id}")])
-            except (IndexError, ValueError):
-                btn.append([InlineKeyboardButton("⟳ Tʀʏ Aɢᴀɪɴ", url=f"https://t.me/{temp.U_NAME}?start={message.command[1]}")])
+                invite_links = []
+                for channel_id in channels:
+                    invite_link = await client.create_chat_invite_link(int(channel_id))
+                    invite_links.append(invite_link.invite_link)
+            except ChatAdminRequired:
+                logger.error("MAKE SURE BOT IS ADMIN IN FORCESUB CHANNEL")
+                return
+            
+            btns = []
+            for invite_link in invite_links:
+                btns.append(InlineKeyboardButton("Join Our Channel", url=invite_link))
+            
+            if message.command[1] != "subscribe":
+                try:
+                    kk, file_id = message.command[1].split("_", 1)
+                    pre = 'checksubp' if kk == 'filep' else 'checksub' 
+                    btns.append(InlineKeyboardButton("⟳ Tʀʏ Aɢᴀɪɴ", callback_data=f"{pre}#{file_id}"))
+                except (IndexError, ValueError):
+                    btns.append(InlineKeyboardButton("⟳ Tʀʏ Aɢᴀɪɴ", url=f"https://t.me/{temp.U_NAME}?start={message.command[1]}"))
+            
+            keyboard = InlineKeyboardMarkup([[btns[0]], [btns[1]], [btns[2]]])  # Each button in a separate row
+            await message.reply_text("Join our channels:", reply_markup=keyboard)
+
                 
         try:
             return await client.send_message(chat_id=message.from_user.id, text=FORCE_SUB_TEXT, reply_markup=InlineKeyboardMarkup(btn), parse_mode=enums.ParseMode.DEFAULT)
